1facde75cbac124f81918d06b230ab6700f8ce58
swongkc13 <swongkc13@gmail.com>
2024-06-08 14:21:57 +0200

    Project Setup & Home Page Build

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import "@styles/globals.css";
+import { Children } from "react";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI propmpts.",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+            {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
