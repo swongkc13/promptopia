1395461714add676c52c6e0c6c554ac058f5564c
swongkc13 <swongkc13@gmail.com>
2024-06-28 18:18:04 +0200

    Completed MongoDB integration & Google Authentication

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    // appDir: true, // Comment this out if it's causing issues
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config
+  }
+};
 
-export default nextConfig;
+export default nextConfig; // Use ES module export syntax
