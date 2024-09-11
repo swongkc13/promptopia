6ec03fccadd937658171eca078e4ea871228d215
swongkc13 <swongkc13@gmail.com>
2024-09-11 17:49:12 +0200

    Added CRUD operations to user profiles

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -9,6 +9,7 @@ import Profile from '@components/profile';
 import React from 'react'
 
 const MyProfile = () => {
+  const router = useRouter();
 
   const { data: session } = useSession();
 
@@ -25,12 +26,26 @@ const MyProfile = () => {
     if(session?.user.id) fetchPosts();
   }, []);
 
-  const handleEdit = () => {
-
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`)
   }
 
-  const handleDelete = async () => {
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
 
+    if(hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`,{
+          method: 'DELETE'
+        })
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+      setPosts(filteredPosts)
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }
 
   return (
